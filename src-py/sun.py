b586f10f2a00c01bb4a2d9f484fde4890ce21728
sauron-acg <none@none>
2010-06-03 18:29:31 +0200

    add sun gadget.

---
+++ src-py/sun.py
@@ -0,0 +1,50 @@
+#!echo "This file is not executable"
+# -*- coding: UTF_8 -*-
+
+#/////////////////////////////////////////////////////////////////////////////////
+# Yet Another Jump'n'Run Game, unfair this time.
+# (c) 2010 Alexander Christoph Gessler
+#
+# HIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ///////////////////////////////////////////////////////////////////////////////////
+
+# PySFML
+import sf
+
+# My own stuff
+import defaults
+from game import Entity,Game,Tile
+from player import Player
+
+class Sun(Tile):
+    """The sun is a nice sun, for it always sticks to the player position"""
+
+    def __init__(self,text,width,height,posy=-0.3):
+        Tile.__init__(self,text,width,height)
+
+        self.posy = posy
+
+    def Interact(self,other,game):
+        return Entity.ENTER
+
+    def Update(self,time_elapsed,time,game):
+        for entity in game._EnumEntities():
+            if isinstance(entity,Player):
+                pos = entity.pos
+                break
+        else:
+            return
+        
+        self.pos = [pos[0] - self.dim[0]//2,self.posy]
+        self.SetPosition(self.pos)
+            
+        
