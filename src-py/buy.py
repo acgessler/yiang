71f605f26489f120d1e3b2c466dc199f75b465b6
sauron-acg <none@none>
2010-06-20 14:57:04 +0200

    Switch font to vera monospace bold.

--- src-py/buy.py
+++ src-py/buy.py
@@ -41,7 +41,6 @@ class OrganTransplantMachine(AnimTile):
 Do you want to buy new organs and have them transplanted immediately 
 to heal your wounds? This gives you an extra life for not more than 
 $ 1! 
-
 This is a truly incredible offer! This is the time to get rid of your
 ill organs and get a fresh body! Note: No orphans were harmed for 
 building this machine. 
@@ -76,8 +75,8 @@ Press {0} to accept the deal (-$1.0, +L) and {1} to leave.
         self.running  = True
         self.game._FadeOutAndShowStatusNotice( sf.String(self.text_formatted,
             Size=defaults.letter_height_game_over,
-            Font=FontCache.get(defaults.letter_height_game_over, face=defaults.font_game_over
-        )),defaults.game_over_fade_time, (625,250) , 0.0, accepted, sf.Color.Green, on_close)
+            Font=FontCache.get(defaults.letter_height_machines, face=defaults.font_machines
+        )),defaults.game_over_fade_time, (640,210) , 0.0, accepted, sf.Color.Red, on_close)
 
     def GetVerboseName(self):
         return "the Instant Organ Transplant Machine (OTM)"
