b96082098d0df1140dd8cf263e42fd7494e31d35
sauron-acg <none@none>
2010-06-22 22:15:17 +0200

    Explain the rationale behind sf.py.

--- src-py/sf.py
+++ src-py/sf.py
@@ -1,1 +1,9 @@
+
+# this is for linux, where the system-wide, regular PySFML is used. 
+# On Windows, PySFML is embedded and put into a single module ('sf')
+# for simplicity (or unawareness of the necessity to port the app
+# at a later time ...). Therefore, on Windoes one may import 'sf'
+# but this module is silently skipped because there is already an
+# init stub entry with this name.
+
 from PySFML.sf import *
