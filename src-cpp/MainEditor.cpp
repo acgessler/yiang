9da606af1fd006935be7a92a5eadf94cfcaea8b0
acg-w7-x64-2 <none@none>
2010-07-26 18:50:45 +0200

    Move PySFML logic to a separate DLL and link the game executable dynastatically against it. Add dummy executable stub to run the editor.
    --HG--
    rename : src-cpp/main.cpp => src-cpp/Main.cpp

---
+++ src-cpp/MainEditor.cpp
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Yet Another Jump'n'Run Game
+// (c) 2010 Alexander Christoph Gessler
+//
+// HIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///////////////////////////////////////////////////////////////////////////////////
+
+// Main entry point, initializes Python and calls editor.py
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#include <string>
+#include <fstream>
+
+// Python C API, PySFML module initialization stub
+#include <Python.h>
+#pragma comment (lib, "Python31.lib")
+#pragma comment (lib, "pysfml.lib")
+
+#else
+
+#include <python3.1/Python.h>
+
+#endif
+
+PyMODINIT_FUNC PyInit_sf(void);
+
+// --------------------------------------------------------------------------------------------
+int PyMain(int argc, wchar_t* argv[])
+{
+	// bootstrapping script, hands control over to editor.py
+	static char start_script_name[] =  "__launch_stub__.py";
+	const char* start_stub = 
+		"import sys\n" 
+		"import traceback\n"
+		"sys.path.append(\'../src-py\')\n"
+		"try:\n"
+		"\timport editor\n"
+		"except Exception as e:\n"
+		"\tprint(e)\n"
+		"\ttraceback.print_exc()\n"
+	;
+#ifdef _MSC_VER
+	PyImport_AppendInittab("sf", & PyInit_sf);
+#endif
+
+	Py_Initialize();   
+
+	PySys_SetArgv(argc, argv);
+
+	const int ret = PyRun_SimpleString(start_stub);
+
+	if (PyErr_Occurred()) {
+		PyErr_Print();
+		PyErr_Clear();
+	}
+
+	Py_Finalize();
+	return ret;
+}
+
+
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	wchar_t** const dat = new wchar_t*[argc]();
+	for(int i = 0; i < argc; ++i) {
+
+		const size_t len = strlen(argv[i]);
+		dat[i] = new wchar_t[len+1]();
+
+		mbstowcs(dat[i],argv[i],len);
+	}
+	
+	const int ret = PyMain(argc,dat);
+
+	for(int i = 0; i < argc; ++i) {
+		delete[] dat[i];
+	}
+	delete[] dat;
+	return ret;
+}
+
+
+
+
+/* vi: set shiftwidth=4 tabstop=4: */ 
---
+++ src-cpp/PySFML_dllmain.cpp
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Yet Another Jump'n'Run Game
+// (c) 2010 Alexander Christoph Gessler
+//
+// HIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///////////////////////////////////////////////////////////////////////////////////
+
+// DllMain stub for the PySFML DLL
+
+#include <Windows.h>
+
+// SFML libs
+#pragma comment (lib, "sfml-graphics.lib")
+#pragma comment (lib, "sfml-window.lib")
+#pragma comment (lib, "sfml-audio.lib")
+#pragma comment (lib, "sfml-system.lib")
+
+// fixup code for some missing SFML symbols
+#include "fixers.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
