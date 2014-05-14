52c0be4a8bacc330412a0a1b9cf816243ed9b454
Alexander Gessler <alexander.gessler@gmx.net>
2014-05-15 01:37:22 +0200

    Add forgotten .rc files.

---
+++ vc9/yiang.h
@@ -0,0 +1,78 @@
+
+#pragma once
+    
+namespace PyCacheTable {
+struct Entry {
+   const wchar_t* name;
+   unsigned int index;
+};
+    
+Entry entries[] = {
+    
+     { L"achievements.pyc", 35189 },
+         { L"archiver.pyc", 36158 },
+         { L"audio.pyc", 53131 },
+         { L"background.pyc", 38664 },
+         { L"buy.pyc", 55338 },
+         { L"campaign.pyc", 39721 },
+         { L"colorchanger.pyc", 63227 },
+         { L"complexanim.pyc", 64894 },
+         { L"cpptools.pyc", 56074 },
+         { L"danger.pyc", 36587 },
+         { L"defaults.pyc", 36008 },
+         { L"editor.pyc", 50270 },
+         { L"editoractions.pyc", 16094 },
+         { L"editormarkup.pyc", 295 },
+         { L"enemy.pyc", 64436 },
+         { L"entity.pyc", 60466 },
+         { L"floatingnotify.pyc", 23823 },
+         { L"fonts.pyc", 36009 },
+         { L"forcefield.pyc", 7266 },
+         { L"fshack.pyc", 25745 },
+         { L"game.pyc", 13622 },
+         { L"genemptylevel.pyc", 5353 },
+         { L"highscore.pyc", 34948 },
+         { L"keys.pyc", 2525 },
+         { L"level.pyc", 45717 },
+         { L"levelup.pyc", 48991 },
+         { L"loadscreen.pyc", 46854 },
+         { L"locked.pyc", 9460 },
+         { L"log.pyc", 27570 },
+         { L"machineparts.pyc", 32640 },
+         { L"main.pyc", 18180 },
+         { L"materials.pyc", 4586 },
+         { L"mathutil.pyc", 6474 },
+         { L"minigui.pyc", 18342 },
+         { L"notification.pyc", 47325 },
+         { L"perks.pyc", 33105 },
+         { L"player.pyc", 24307 },
+         { L"posteffect.pyc", 46631 },
+         { L"preboot.pyc", 55408 },
+         { L"quiz.pyc", 38397 },
+         { L"renderer.pyc", 28154 },
+         { L"score.pyc", 1654 },
+         { L"sf.pyc", 32970 },
+         { L"stubs.pyc", 24281 },
+         { L"studiosplash.pyc", 11846 },
+         { L"teleport.pyc", 28755 },
+         { L"textures.pyc", 20445 },
+         { L"tile.pyc", 54020 },
+         { L"title.pyc", 6996 },
+         { L"tutorial.pyc", 18327 },
+         { L"updaters\\ASPECT.pyc", 33065 },
+         { L"updaters\\DITHER_STRENGTH.pyc", 5997 },
+         { L"updaters\\FRAMEBUFFER.pyc", 51008 },
+         { L"updaters\\PLAYER_POS.pyc", 6679 },
+         { L"updaters\\RANDOM.pyc", 4741 },
+         { L"updaters\\STATUS_BAR_DIM.pyc", 39123 },
+         { L"updaters\\__init__.pyc", 61212 },
+         { L"userprofile.pyc", 4381 },
+         { L"validator.pyc", 51234 },
+         { L"weapon.pyc", 22646 },
+         
+       {0,0} // end sentinel
+    };
+}
+    
+#define NUM_PYCACHE_ENTRIES 60
+    
